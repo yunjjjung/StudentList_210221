4dffda74cf1fac8dca4e48148dfdcb84474b7590
최윤정 <jjjung1003@naver.com>
2021-02-28 09:50:49 +0900

    어댑터 / xml / ArrayList 조합 => 리스트뷰에 부착

--- app/src/main/java/com/thejoeun/studentlist_210221/MainActivity.kt
+++ app/src/main/java/com/thejoeun/studentlist_210221/MainActivity.kt
@@ -2,12 +2,17 @@ package com.thejoeun.studentlist_210221
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.thejoeun.studentlist_210221.adapters.StudentAdapter
 import com.thejoeun.studentlist_210221.datas.Student
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
 //    실제 학생들을 담아줄 목록 변수.
     val mStudentList = ArrayList<Student>()
+
+//    리스트 뷰에 각각의 줄을 실제 뿌려줄 어댑터 변수
+    lateinit var mAdapter : StudentAdapter   //변수인데 나중에 대입할 변수 (onCreate 실행 이후)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +29,12 @@ class MainActivity : AppCompatActivity() {
         mStudentList.add(Student("채명", 1973))
         mStudentList.add(Student("최원", 1967))
         mStudentList.add(Student("최윤", 1987))
+
+//        나중에 담아준다던 mAdapter에 실제 어댑터 대입
+        mAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList)   // 이 화면에서 student_list_item 모양으로 mStudentList 을 뿌려라
+
+//        리스트 뷰의 어댑터로써 => mAdapter를 지정
+        studentListView.adapter = mAdapter
 
 
 
