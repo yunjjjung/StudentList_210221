3185e353f148229c6dffa3b05b587c849343304c
최윤정 <jjjung1003@naver.com>
2021-02-28 10:10:46 +0900

    어댑터 - getView 함수 수정 (실 데이터 반영)  - 나이는 아직 제대로 계산되지 않은 상태

--- app/src/main/java/com/thejoeun/studentlist_210221/adapters/StudentAdapter.kt
+++ app/src/main/java/com/thejoeun/studentlist_210221/adapters/StudentAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.thejoeun.studentlist_210221.R
 import com.thejoeun.studentlist_210221.datas.Student
 
@@ -16,7 +17,7 @@ class StudentAdapter(
 //    student_list_item xml을 가지고 => 코틀린에서 다룰 수 있게 도와주는 변수. (inf)클래스 (LayoutInflater)
     val inf = LayoutInflater.from(mContext)
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {  //position이라는 변수를 Int로 데려옴
 
         var tempRow = convertView   //임시로 한칸을 그려넣겠다
 
@@ -25,6 +26,23 @@ class StudentAdapter(
         }
 
         val row = tempRow!! //절대로 안 비어있어
+
+//        화면에 뿌려지는 row를 일부 수정하고 나서 => return 되도록.
+//        몇번째 줄에 뿌릴 row인지 파악하고 상황에 맞게 가공.
+//        mList에서 위치에 맞는 학생을 먼저 추출.
+
+        val student = mList[position]
+
+//        raw변수 (XML모양)에서 => 필요한 텍스트뷰/이미지뷰를 꺼내오자 (수기작업)
+
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+        val ageTxt = row.findViewById<TextView>(R.id.ageTxt)
+
+//        Student의 값을 => 텍스트뷰들에 반영
+
+        nameTxt.text = student.name
+        ageTxt.text = "(${student.birthYear}세)"
+
 
         return row
     }
